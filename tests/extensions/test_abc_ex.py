a49b066d6eae79583143dfdc08ed90102aef8a62
imhuay <imhuay@163.com>
2022-09-25 02:02:51 +0800

    Update test_abc_ex.py: remove test_time_cost

--- tests/extensions/test_abc_ex.py
+++ tests/extensions/test_abc_ex.py
@@ -13,7 +13,6 @@ from __future__ import annotations
 # import os
 # import sys
 # import json
-import time
 import unittest
 
 # from typing import *
@@ -21,6 +20,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from huaytools.extensions import SingletonMeta
 from huaytools.extensions import SingletonABC
+
 
 # from huaytools.utils import get_logger
 # logger = get_logger()
@@ -59,48 +59,49 @@ class TestSingletonMeta(unittest.TestCase):
         for i in instances:
             self.assertIs(i, singleton)
 
-    def test_time_cost(self):
-        """
-        测试显示加入两次判断，能够降低时间
-        """
-        from threading import Lock
-
-        class _SingletonMeta(type):
-            _instances = dict()
-            _lock = Lock()
-
-            def __call__(cls, *args, **kwargs):
-                # 去除第一次判断
-                # if cls not in cls._instances:
-                with cls._lock:
-                    if cls not in cls._instances:  # 第二次判断：防止实例被多次创建
-                        # import time
-                        # time.sleep(1)  # test thread safety
-                        cls._instances[cls] = super().__call__(*args, **kwargs)
-                return cls._instances[cls]
-
-        class A(metaclass=SingletonMeta):
-            pass
-
-        class B(metaclass=_SingletonMeta):
-            pass
-
-        n_time = 100000
-        tmp = []
-        start = time.time()
-        for _ in range(n_time):
-            tmp.append(A())
-        t1 = time.time() - start
-        # print()
-        # print(t1)
-
-        tmp = []
-        start = time.time()
-        for _ in range(n_time):
-            tmp.append(B())
-        t2 = time.time() - start
-        # print(t2)
-        self.assertLess(t1, t2)
+    # def test_time_cost(self):
+    #     """
+    #     测试显示加入两次判断，是否能够提升速度
+    #     """
+    #     import time
+    #     from threading import Lock
+    #
+    #     class _SingletonMeta(type):
+    #         _instances = dict()
+    #         _lock = Lock()
+    #
+    #         def __call__(cls, *args, **kwargs):
+    #             # 去除第一次判断
+    #             # if cls not in cls._instances:
+    #             with cls._lock:
+    #                 if cls not in cls._instances:  # 第二次判断：防止实例被多次创建
+    #                     # import time
+    #                     # time.sleep(1)  # test thread safety
+    #                     cls._instances[cls] = super().__call__(*args, **kwargs)
+    #             return cls._instances[cls]
+    #
+    #     class A(metaclass=SingletonMeta):
+    #         pass
+    #
+    #     class B(metaclass=_SingletonMeta):
+    #         pass
+    #
+    #     n_time = 100000
+    #     tmp = []
+    #     start = time.time()
+    #     for _ in range(n_time):
+    #         tmp.append(A())
+    #     t1 = time.time() - start
+    #     # print()
+    #     # print(t1)
+    #
+    #     tmp = []
+    #     start = time.time()
+    #     for _ in range(n_time):
+    #         tmp.append(B())
+    #     t2 = time.time() - start
+    #     # print(t2)
+    #     self.assertLess(t1, t2)
 
 
 class TestSingletonABC(TestSingletonMeta):
