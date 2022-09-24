5fbdb2dbe6678bb590e1a2d5a77f92dfec1f7fdd
imhuay <imhuay@163.com>
2022-09-24 23:37:45 +0800

    Update project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Time:
+    2022-09-25 0:13
+Author:
+    huayang (imhuay@163.com)
+Subject:
+    setup.py
+"""
+from setuptools import setup
+
+setup()
